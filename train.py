ec18c87d6275d3a5be2433df4a7c3e66e09e5915
Kwak-JunYoung <klop9090@g.skku.edu>
2023-03-23 23:07:21 +0900

    feat: Implementation of train.py. May be incomplete

---
+++ train.py
@@ -0,0 +1,46 @@
+import torch
+from tqdm import tqdm, tqdm_notebook
+
+def calc_accuracy(X,Y):
+    max_vals, max_indices = torch.max(X, 1)
+    train_acc = (max_indices == Y).sum().data.cpu().numpy()/max_indices.size()[0]
+    return train_acc
+
+def model_train(model, config, train_dataloader, test_dataloader, scheduler):
+    num_epochs = config["train_config"]["num_epochs"]
+    optimizer = config["train_config"]["optimizer"]
+    log_interval = config["train_config"]["log_interval"]
+    max_grad_norm = config["train_config"]["max_grad_norm"]
+    loss_fn = torch.nn.CrossEntropyLoss()
+
+
+    for e in range(num_epochs):
+        train_acc = 0.0
+        test_acc = 0.0
+        model.train()
+        for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(tqdm_notebook(train_dataloader)):
+            optimizer.zero_grad()
+            token_ids = token_ids.long().to(device)
+            segment_ids = segment_ids.long().to(device)
+            valid_length= valid_length
+            label = label.long().to(device)
+            out = model(token_ids, valid_length, segment_ids)
+            loss = loss_fn(out, label)
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+            optimizer.step()
+            scheduler.step()  # Update learning rate schedule
+            train_acc += calc_accuracy(out, label)
+            if batch_id % log_interval == 0:
+                print("epoch {} batch id {} loss {} train acc {}".format(e+1, batch_id+1, loss.data.cpu().numpy(), train_acc / (batch_id+1)))
+        print("epoch {} train acc {}".format(e+1, train_acc / (batch_id+1)))
+        
+        model.eval()
+        for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(tqdm_notebook(test_dataloader)):
+            token_ids = token_ids.long().to(device)
+            segment_ids = segment_ids.long().to(device)
+            valid_length= valid_length
+            label = label.long().to(device)
+            out = model(token_ids, valid_length, segment_ids)
+            test_acc += calc_accuracy(out, label)
+        print("epoch {} test acc {}".format(e+1, test_acc / (batch_id+1)))
