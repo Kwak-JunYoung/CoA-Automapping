67f4d7508ebd56f1429b038708a16efef718c117
Kwak-JunYoung <klop9090@g.skku.edu>
2023-03-27 16:31:02 +0900

    Fix: Consideration of account code

--- preprocess_data.py
+++ preprocess_data.py
@@ -4,14 +4,13 @@ from argparse import ArgumentParser
 
 BASE_PATH = "./data"
 
-def prepareDataset(data_name: str):
+def prepare_samilCoA(data_name: str):
     # only SamilCoA2023 for now
     # data_name can be admin_dis / company_admin
-    coa_name = "SamilCoA2023.xlsx"
-    data_path = os.path.join(BASE_PATH, coa_name)
-    headers = ["회사계정", "1차번역", "계정과목", "관리계정", "공시용계정", "합산계정", "분류", "구분"]
-    comp_admin_headers = ["1차번역", "관리계정"]
-    admin_dis_headers = ["관리계정", "공시용계정"]
+    data_path = os.path.join(BASE_PATH, data_name)
+    headers = ["계정코드", "회사계정", "1차번역", "계정과목", "관리계정", "공시용계정", "합산계정", "분류", "구분"]
+    comp_admin_headers = ["계정코드", "1차번역", "관리계정"]
+    admin_dis_headers = ["계정코드", "관리계정", "공시용계정"]
     
     usecols = []
 
@@ -20,7 +19,7 @@ def prepareDataset(data_name: str):
     elif data_name == "admin_dis":
         usecols = admin_dis_headers
     
-    df = pd.read_excel("data/SamilCoA2023.xlsx", usecols=usecols)
+    df = pd.read_excel(os.path.join(data_path, "SamilCoA2023.xlsx"), usecols=usecols)
 
     # if data_name == "company_admin":
     #     df = df.drop_duplicates("1차번역")
@@ -28,9 +27,9 @@ def prepareDataset(data_name: str):
     #     df = df.drop_duplicates("관리계정")
     
     if data_name == "company_admin":
-        df.to_excel(os.path.join(data_path, "comp_admin.xlsx"), index=False)
+        df.to_excel(os.path.join(data_path, "preprocessed_df.xlsx"), index=False)
     elif data_name == "admin_dis":
-        df.to_excel(os.path.join(data_path, "admin_dis.xlsx"), index=False)
+        df.to_excel(os.path.join(data_path, "preprocessed_df.xlsx"), index=False)
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Preprocess CoA dataset")
