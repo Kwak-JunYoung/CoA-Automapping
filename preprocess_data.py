59a5be73d3294a0abe14958ae80cb053388639c6
Kwak-JunYoung <klop9090@g.skku.edu>
2023-03-29 13:14:00 +0900

    Feat: Implementation of data preprocessing code in 5 types

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,6 +1,8 @@
 import os
-from pandas import pd
 from argparse import ArgumentParser
+import pandas as pd
+import numpy as np
+from IPython.display import display 
 
 BASE_PATH = "./data"
 
@@ -8,19 +10,17 @@ BASE_PATH = "./data"
 def get_dist_info(
     originalDf
 ):
-    dist_df = pd.DataFrame()
-    dist_df["공시용계정"] = originalDf["공시용계정"].drop_duplicates(keep="first")
+    dist_list = originalDf["공시용계정"].drop_duplicates(keep="first").to_list()
+    dist_dict = {}
+    print(len(dist_list))
+    for i in range(len(dist_list)):
+        dist_dict[dist_list[i]] = i
 
-    dist_df["enum"] = [i for i in range(len(dist_df["공시용계정"]))] 
+    return dist_dict
 
-    return dist_df
-
-def prepare_samilCoA(data_name: str, preprocess_type: str):
+def prepare_samilCoA(file_name: str, data_path: str, preprocess_type: str):
     # comp, abs, plain, part
     # This data will have headers as mentioned below
-    file_name = "SamilCoA2023(2).xlsx"
-    data_name = "SamilCoA2023"
-    data_path = os.path.join(BASE_PATH, data_name)
     # headers = ["계정코드", "회사계정", "1차번역", "계정과목", "관리계정", "공시용계정", "합산계정", "분류", "구분", "회사명", ]
     comp_admin_headers = ["계정코드", "1차번역", "관리계정"]
 
@@ -30,10 +30,14 @@ def prepare_samilCoA(data_name: str, preprocess_type: str):
     part_admin_dis_headers = ["계정코드", "관리계정", "공시용계정", "회사명"]
     
     originalDf = pd.read_excel(os.path.join(data_path, file_name))
-    originalDf.loc[:,["계정과목", "관리계정", "공시용계정"]].replace(" ", "")
+    originalDf["공시용계정"] = originalDf["공시용계정"].str.replace(' ', '')
+    originalDf["계정과목"] = originalDf["계정과목"].str.replace(' ', '')
+    originalDf["관리계정"] = originalDf["관리계정"].str.replace(' ', '')
 
-    # Dist accnt needs to be enumerated
-    dist_enum = get_dist_info(originalDf=originalDf)
+    # List-ified independent distribution accounts
+    # Enumerating dist-accnt by using .index will do the trick
+    dist_dict = get_dist_info(originalDf=originalDf)
+    originalDf["공시용계정"] = originalDf["공시용계정"].map(dist_dict)
 
     usecols = []
 
@@ -64,7 +68,6 @@ def prepare_samilCoA(data_name: str, preprocess_type: str):
     elif preprocess_type == "part_admin_dis":
         df.to_excel(os.path.join(data_path, "part_admin_dis_df.xlsx"), index=False)
 
-
 if __name__ == "__main__":
     file_name = "SamilCoA2023(2).xlsx"
     data_name = "SamilCoA2023"
@@ -74,4 +77,10 @@ if __name__ == "__main__":
     parser.add_argument("--preprocess_type", type=str, default="admin_dis")
     args = parser.parse_args()
 
-    prepare_samilCoA(data_name=data_name, preprocess_type=args.preprocess_type)
+    originalDf = pd.read_excel(os.path.join(data_path, file_name))
+
+    # Dist accnt needs to be enumerated
+    dist_dict = get_dist_info(originalDf=originalDf)
+    # print(dist_dict)
+
+    prepare_samilCoA(file_name=file_name, data_path=data_path, preprocess_type=args.preprocess_type)
