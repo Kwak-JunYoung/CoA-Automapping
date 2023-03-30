fe7810c3693f5851d1376bb09cc13d30969225b8
Kwak-JunYoung <klop9090@g.skku.edu>
2023-03-30 20:08:33 +0900

    Feat: Learning will be made under 5 different conditions
    - Based on preprocess types

--- run_model.sh
+++ run_model.sh
@@ -1,8 +1,8 @@
-data=(admin_dis part_admin_dis)
+data=(admin_dis part_admin_dis comp_admin_dis plain_admin_dis abs_admin_dis)
 
 for d in ${data[@]}; do
     args=(
         --preprocess_type ${d}
     )
-    echo `python preprocess_data.py ${args[@]}`
+    echo `python main.py ${args[@]}`
 done
