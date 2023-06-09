5f63427f2226e8f9f3700e4ac3ff3173ebb4b132
Kwak-JunYoung <klop9090@g.skku.edu>
2023-06-09 15:13:41 +0900

    Fix: Stringify parts of data.

--- data_loader.py
+++ data_loader.py
@@ -6,7 +6,7 @@ class BERTDataset(Dataset):
     def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, max_len, pad, pair):
         transform = nlp.data.BERTSentenceTransform(bert_tokenizer, max_seq_length=max_len, pad=pad, pair=pair)
 
-        self.sentences = [transform([i[sent_idx]]) for i in dataset]
+        self.sentences = [transform([str(i[sent_idx])]) for i in dataset]
         self.labels = [np.int32(i[label_idx]) for i in dataset]
 
     def __getitem__(self, i):
