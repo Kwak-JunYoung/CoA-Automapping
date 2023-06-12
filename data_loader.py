0d955b053d451e06702b104ed3257de5a7f08052
Kwak-JunYoung <klop9090@g.skku.edu>
2023-06-12 15:19:24 +0900

    Fix: Re-addition of vocab parameter, will find a way of how to utilize gluonnlp==0.10.0 properly.

--- data_loader.py
+++ data_loader.py
@@ -3,8 +3,8 @@ import numpy as np
 from torch.utils.data import Dataset
 
 class BERTDataset(Dataset):
-    def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, max_len, pad, pair):
-        transform = nlp.data.BERTSentenceTransform(bert_tokenizer, max_seq_length=max_len, pad=pad, pair=pair)
+    def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, vocab, max_len, pad, pair):
+        transform = nlp.data.BERTSentenceTransform(bert_tokenizer, max_seq_length=max_len, vocab=vocab, pad=pad, pair=pair)
 
         self.sentences = [transform([i[sent_idx]]) for i in dataset]
         self.labels = [np.int32(i[label_idx]) for i in dataset]
